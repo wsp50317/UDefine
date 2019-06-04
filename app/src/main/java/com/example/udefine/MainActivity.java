4c6d908b83bf96683674edbb4c369f4e5ff6ed7a
oiz5201618 <oiz5201618@gmail.com>
2019-06-04 20:50:22 +0800

    Implement recyclerview prototype in Main Activity 1. Test data should be remove later. 2. Remove useless file .gradle/ .idea/

--- app/src/main/java/com/example/udefine/MainActivity.java
+++ app/src/main/java/com/example/udefine/MainActivity.java
@@ -4,20 +4,36 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+
+    // TODO:testing data, should be remove later.
+    private final LinkedList<String> mNoteTitleList = new LinkedList<>();
+    private final LinkedList<String> mNoteTimeList = new LinkedList<>();
+    private final LinkedList<String> mNoteTagList = new LinkedList<>();
+
+
+    private RecyclerView mRecyclerView;
+    private NoteListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Default toolbar setting
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Default FAB setting
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +42,23 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Put initial data into the word list.
+        for (int i = 0; i < 20; i++) {
+            mNoteTitleList.addLast("Title-" + i);
+            mNoteTimeList.addLast("Time:" + i);
+            mNoteTagList.addLast("Tag-" + i);
+        }
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.note_list_recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new NoteListAdapter(this, mNoteTitleList, mNoteTimeList, mNoteTagList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
     @Override
