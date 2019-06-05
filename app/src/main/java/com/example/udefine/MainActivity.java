e2c7deb6ce4715324af96e17ba4a6943ffc2a7d4
ShaoHua Wang <oiz5201618@gmail.com>
2019-06-05 17:22:07 +0800

    Connect Main and NewNote Activity (#10)

--- app/src/main/java/com/example/udefine/MainActivity.java
+++ app/src/main/java/com/example/udefine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.udefine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                Intent intent = new Intent(MainActivity.this, NewNote.class);
+//                String message = mMessageEditText.getText().toString();
+//                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
             }
         });
 
