084393c9cbc653170dcbefab8f9d7a13c6136e47
jkrvivian <jkrvivian@gmail.com>
2019-06-08 02:12:25 +0800

    Implement NewNote basic UI (#21)

--- app/src/main/java/com/example/udefine/NewNote.java
+++ app/src/main/java/com/example/udefine/NewNote.java
@@ -1,20 +1,49 @@
 package com.example.udefine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NewNote extends AppCompatActivity {
 
+    private Spinner mSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // tag spinner settings
+        final String[] tag_colors = {
+                "Select Tag", "#00ff00", "#ffff00", "#ff751a", "#ff0000",
+                "#993399", "#6666ff"};
+        mSpinner = findViewById(R.id.tag_spinner);
+
+        ArrayList<tagItemStateVO> listVOs = new ArrayList<>();
+
+        for (int i = 0; i < tag_colors.length; i++) {
+            tagItemStateVO stateVO = new tagItemStateVO();
+            stateVO.setTitle(tag_colors[i]);
+            stateVO.setSelected(false);
+            listVOs.add(stateVO);
+        }
+        tagListAdapter myAdapter = new tagListAdapter(NewNote.this, 0,
+                listVOs);
+        mSpinner.setAdapter(myAdapter);
 
         /*
          *  1. load selected layout
@@ -28,4 +57,24 @@ public class NewNote extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(NewNote.this, LayoutSelection.class);
+        startActivity(intent);
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void datePicker(View view) {
+    }
+
+    public void timePicker(View view) {
+    }
 }
---
+++ app/src/main/java/com/example/udefine/tagListAdapter.java
@@ -0,0 +1,94 @@
+package com.example.udefine;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class tagListAdapter extends ArrayAdapter<tagItemStateVO> {
+    private Context mContext;
+    private ArrayList<tagItemStateVO> listState;
+    private tagListAdapter myAdapter;
+    private boolean isFromView = false;
+
+    public tagListAdapter(Context context, int resource, List<tagItemStateVO> objects) {
+        super(context, resource, objects);
+        this.mContext = context;
+        this.listState = (ArrayList<tagItemStateVO>) objects;
+        this.myAdapter = this;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView,
+                                ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    public View getCustomView(final int position, View convertView,
+                              ViewGroup parent) {
+
+        final ViewHolder holder;
+        if (convertView == null) {
+            LayoutInflater layoutInflator = LayoutInflater.from(mContext);
+            convertView = layoutInflator.inflate(R.layout.tag_spinner_layout, null);
+            holder = new ViewHolder();
+            holder.mTextView = convertView.findViewById(R.id.tag_color);
+            holder.mCheckBox = convertView.findViewById(R.id.tag_checkbox);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+
+        if (position != 0) {
+            holder.mTextView.setBackgroundColor(Color.parseColor(listState.get(position).getTitle()));
+        } else {
+            holder.mTextView.setText(listState.get(0).getTitle());
+        }
+
+        // To check weather checked event fire from getview() or user input
+        isFromView = true;
+        holder.mCheckBox.setChecked(listState.get(position).isSelected());
+        isFromView = false;
+
+
+        if ((position == 0)) {
+            holder.mCheckBox.setVisibility(View.INVISIBLE);
+        } else {
+            holder.mCheckBox.setVisibility(View.VISIBLE);
+        }
+
+        holder.mCheckBox.setTag(position);
+        holder.mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int getPosition = (Integer) buttonView.getTag();
+
+                if (!isFromView) {
+                    listState.get(position).setSelected(isChecked);
+                }
+            }
+        });
+        return convertView;
+    }
+
+    private class ViewHolder {
+        private TextView mTextView;
+        private CheckBox mCheckBox;
+    }
+}
