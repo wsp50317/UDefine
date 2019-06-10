9db3d88b8aef930059a3e08acea8bb3fb8475b8f
joy45689 <44458778+joy45689@users.noreply.github.com>
2019-06-10 10:31:07 +0800

    Populate the database (#29)
    Update "MyRoomDatabase.java" by adding an inner class "PopulateDbAsync"

--- app/src/main/java/com/example/udefine/Database/MyRoomDatabase.java
+++ app/src/main/java/com/example/udefine/Database/MyRoomDatabase.java
@@ -1,16 +1,58 @@
 package com.example.udefine.Database;
 
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {NoteList.class, LayoutList.class,Notes.class,Layouts.class}, version = 1, exportSchema = false)
 public abstract class MyRoomDatabase extends RoomDatabase {
 
     public abstract MyDao myDao();
     private static MyRoomDatabase INSTANCE;
+    private static RoomDatabase.Callback roomDatabaseCallbacl =
+            new RoomDatabase.Callback(){
+                @Override
+                public void onOpen(@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+
+                }
+            };
+
+    /**
+     * Populate the database in the background.
+     */
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final MyDao mDao;
+        String [] title = {"買菜","搶養唱會門票","作業Demo"};
+
+        PopulateDbAsync(MyRoomDatabase db) {
+            mDao = db.myDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate the database
+            // when it is first created
+            mDao.deleteAllNotes();
+            mDao.deleteAllNoteList();
+
+            for (int i = 0; i <= title.length - 1; i++) {
+                NoteList noteList = new NoteList(title[i],"5/29","0",1);
+                mDao.insertNotelist(noteList);
+                Notes notes = new Notes(mDao.getLastNoteID(),"標題",title[i]);
+                mDao.insertNote(notes);
+            }
+            return null;
+        }
+    }
 
     static MyRoomDatabase getDatabase(final Context context) {
         //TODO migration的問題
@@ -23,6 +65,7 @@ public abstract class MyRoomDatabase extends RoomDatabase {
                             // if no Migration object.
                             // Migration is not part of this practical.
                             .fallbackToDestructiveMigration()
+                            .addCallback(roomDatabaseCallbacl)
                             .build();
                 }
             }
